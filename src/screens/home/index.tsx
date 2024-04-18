349c294d918faf19d54e858d19ce4fb33915d668
marcos Breno Erlacher <marcosbrenoerlacher@MacBook-Air-de-marcos.local>
2024-04-18 17:06:30 -0300

    primeiro commit

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Dev reels</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
